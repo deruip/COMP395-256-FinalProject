d2f0625af8d95b0c4f4f1f560861482b5b971cf7
Nacocan <morganlindner@ymail.com>
2019-04-15 12:44:53 -0400

    Add files via upload

---
+++ GoodBoy/Scripts/DogAcademy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class DogAcademy : Academy
+{
+    private DogArea[] areas;
+
+    /// <summary>
+    /// Reset the academy
+    /// </summary>
+    public override void AcademyReset()
+    {
+        if (areas == null)
+        {
+            areas = GameObject.FindObjectsOfType<DogArea>();
+        }
+
+        foreach (DogArea area in areas)
+        {
+
+            area.numTruffles = (int)resetParameters["num_truffles"];
+            area.numStumps = (int)resetParameters["num_stumps"];
+            area.spawnRange = resetParameters["spawn_range"];
+
+            area.ResetArea();
+        }
+    }
+}
